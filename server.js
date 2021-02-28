5f5a102858d29ae8ca203fcc31482d35fa225c80
BontigaoDavid <bontigaodavid@gmail.com>
2021-02-28 05:15:55 -0600

    express

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const generatePassword = require('password-generator');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
@@ -24,9 +24,13 @@ app.get('/api/passwords', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+app.get('*', function(_, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 
 const port = process.env.PORT || 5000;
 app.listen(port);
