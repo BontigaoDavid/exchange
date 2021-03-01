bea8739ccd4f41054638b19694967b50d0aff82f
BontigaoDavid <bontigaodavid@gmail.com>
2021-03-01 01:40:30 -0600

    user database controller

--- api/users/index.js
+++ api/users/index.js
@@ -2,7 +2,7 @@ let express = require("express");
 let router = express.Router();
 let controller = require("./users.js")
 
-router.get("/", controller.get);
+router.get("/:email", controller.get);
 router.post("/", controller.post);
 router.put("/", controller.put);
 router.delete("/", controller.delete);
--- api/users/users.js
+++ api/users/users.js
@@ -1,12 +1,26 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('../../firebase-private-key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
 
 let controller = {
-    get: (req, res) => {
-        res.send("API/USERS/ get RESPONSE")
+    get: async (req, res) => {
+        let email = req.params.email;
+
+        let user = (await db.collection("users").doc(email).get()).data();
+        
+        res.send(user.firstname);
     },
-    post: (req, res) => {
+    post: async (req, res) => {
         let user = req.body;
-        console.log(user);
-        res.send("api/users post response");
+
+        await db.collection("users").doc(user.email).set(user);
+        res.send("user created \n" + JSON.stringify(user));
     },
     put: (req, res) => {
         res.send("API/USERS/ put RESPONSE")
